c8085aabc436c31a9d75caa9037321c910010db2
lalailac <preciousballaso2@gmail.com>
2025-06-12 20:43:18 +0800

    Create NoteViewModel.kt

---
+++ NoteViewModel.kt
@@ -0,0 +1,10 @@
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: NoteRepository
+    val allNotes: LiveData<List<Note>>
+
+    init {
+        val dao = NoteDatabase.getDatabase(application).noteDao()
+        repository = NoteRepository(dao)
+        allNotes = repository.allNotes
+    }
+}
