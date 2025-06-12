49c0cc98eec906a81ccf37d0a4f50778af865d90
lalailac <preciousballaso2@gmail.com>
2025-06-12 20:42:18 +0800

    Create NoteDatabase.kt

---
+++ NoteDatabase.kt
@@ -0,0 +1,20 @@
+@Database(entities = [Note::class], version = 1)
+abstract class NoteDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        @Volatile private var INSTANCE: NoteDatabase? = null
+
+        fun getDatabase(context: Context): NoteDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDatabase::class.java,
+                    "note_database"
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
